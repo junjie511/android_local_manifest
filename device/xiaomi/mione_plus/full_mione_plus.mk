2f94afe7898bdab6814f64493302476113b1627e
enoodle <junjie511@gmail.com>
2013-08-18 17:27:48 +0800

    device for mione aosp4.3

---
+++ device/xiaomi/mione_plus/full_mione_plus.mk
@@ -0,0 +1,50 @@
+# Specify phone tech before including full_phone
+# $(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := mione_plus
+
+TARGET_BOOTANIMATION_NAME := vertical-480x854
+
+# Inherit some common CM stuff.
+# $(call inherit-product, vendor/cm/config/common.mk)
+
+# old audio files
+include frameworks/base/data/sounds/AllAudio.mk
+
+# Default ringtone
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.ringtone=Ring_Digital_02.ogg \
+    ro.config.notification_sound=F1_New_SMS.ogg \
+    ro.config.alarm_alert=Alarm_Beep_03.ogg
+
+PRODUCT_PACKAGES += \
+    HoloSpiralWallpaper \
+    MagicSmokeWallpapers \
+    NoiseField \
+    Galaxy4 \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    VisualizationWallpapers \
+    PhaseBeam \
+    Firewall \
+    Mms
+
+# Inherit device configuration
+$(call inherit-product, device/xiaomi/mione_plus/aosp_mione_plus.mk)
+
+# Setup device specific product configuration.
+PRODUCT_DEVICE := mione_plus
+PRODUCT_NAME := full_mione_plus
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI-ONE Plus
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_CHARACTERISTICS := phone
+
+# Set build fingerprint / ID / Product Name etc.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	PRODUCT_NAME=mione_plus \
+	BUILD_FINGERPRINT="Xiaomi/mione_plus/mione_plus:4.1.2/JZO54K/QDS84:userdebug/test-keys" \
+	PRIVATE_BUILD_DESC="mione_plus-userdebug 4.1.2 JZO54K QDS84 test-keys"
+
